eafacf7caa6f04ee03d5367e96dcafd8f249773f
陳又瑜 <91246393+youyu0@users.noreply.github.com>
2023-11-06 17:25:35 +0800

    Create fisheye.py

---
+++ fisheye.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cv2
+from google.colab.patches import cv2_imshow
+
+def fisheye_effect( f ):
+	nr, nc = f.shape[:2]
+	map_x = np.zeros( [nr, nc], dtype = 'float32' )
+	map_y = np.zeros( [nr, nc], dtype = 'float32' )
+	x0, y0 = nr // 2, nc // 2
+	R = np.sqrt( nr ** 2 + nc ** 2 ) / 2
+	for x in range( nr ):
+		for y in range( nc ):
+			r = np.sqrt( ( x - x0 ) ** 2 + ( y - y0 ) ** 2 )
+			if r == 0:  theta = 0
+			else:		theta = np.arccos( ( x - x0 ) / r )
+			r = ( r * r ) / R
+			if y - y0 < 0:  theta = -theta
+			map_x[x,y] = np.clip( y0 + r * np.sin( theta ), 0, nc - 1 )
+			map_y[x,y] = np.clip( x0 + r * np.cos( theta ), 0, nr - 1 )
+	g = cv2.remap( f, map_x, map_y, cv2.INTER_CUBIC )
+	return g
+
+def main( ):
+	img1 = cv2.imread( "output.png", -1 )
+	img2 = fisheye_effect( img1 )
+	cv2_imshow(  img1 )
+	cv2_imshow( img2 )
+	
+
+main( )
